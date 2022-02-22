efd0f872d0c208dad8e058e107638133eac61f3a
Alex <your@email.com>
2022-02-22 14:15:16 +0000

    added nodemon dependency

--- express_server.js
+++ express_server.js
@@ -55,8 +55,18 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new")
 })
 
+// Redirect short URL to long...
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL)
+})
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  const shortURL = generateRandomString()
+  console.log(req.body.longURL)
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+  };
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
