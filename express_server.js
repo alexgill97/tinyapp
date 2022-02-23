d3c3fabf0c8257503fc818f6148cdd46abc36fa4
Alex <your@email.com>
2022-02-23 23:29:24 +0000

    added registration page and get method

--- express_server.js
+++ express_server.js
@@ -90,6 +90,12 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls")
 })
 
+//Registration page GET & Render
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_registration", templateVars)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
